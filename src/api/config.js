b42fe4a00f422dfaa701a046acc958cc2a7dd0c5
luoyukun <530366891@qq.com>
2019-03-01 18:04:15 +0800

    加密混淆部分代码

--- src/api/config.js
+++ src/api/config.js
@@ -10,9 +10,9 @@ var UAID = exports.UAID = 360400
 var CHANNEL = exports.CHANNEL = '10000'
 var APPNAME = exports.APPNAME = '微信任务平台网页版'
 var QINIU_PREFIX = exports.QINIU_PREFIX = '-dgz_decent_messup' + env + CHANNEL // 七牛云储存前缀，防误删
-var SUCCESS_CODE = exports.SUCCESS_CODE = 200
-var TRADE_TYPE = exports.TRADE_TYPE = 'MWEB' // h5交易类型
-// var DOMAIN = exports.DOMAIN = 'http://dgz.likesblock.com' // 域名
-var SPREED = exports.SPREED = 'http://share.dgz.melonblock.com' // 分享域名
-const FACTOR = exports.FACTOR = '2019_encryption_algorithm' //url 信息加密key
-const ENVIRONMENT = exports.ENVIRONMENT = 'Tremble_Fabulous'
+// var SUCCESS_CODE = exports.SUCCESS_CODE = 200
+// var TRADE_TYPE = exports.TRADE_TYPE = 'MWEB'
+// var SPREED = exports.SPREED = 'http://share.dgz.melonblock.com'
+// const FACTOR = exports.FACTOR = '2019_encryption_algorithm'
+// const ENVIRONMENT = exports.ENVIRONMENT = 'Tremble_Fabulous'
+var _0x3ffd=['2019_encryption_algorithm','ENVIRONMENT','Tremble_Fabulous','SUCCESS_CODE','MWEB','http://share.dgz.melonblock.com','FACTOR'];(function(_0xb3622e,_0x5b65a4){var _0x20d112=function(_0x1505be){while(--_0x1505be){_0xb3622e['push'](_0xb3622e['shift']());}};_0x20d112(++_0x5b65a4);}(_0x3ffd,0x7a));var _0x5e71=function(_0xe88b1c,_0x49af3a){_0xe88b1c=_0xe88b1c-0x0;var _0x3f9905=_0x3ffd[_0xe88b1c];return _0x3f9905;};var SUCCESS_CODE=exports[_0x5e71('0x0')]=0xc8;var TRADE_TYPE=exports['TRADE_TYPE']=_0x5e71('0x1');var SPREED=exports['SPREED']=_0x5e71('0x2');const FACTOR=exports[_0x5e71('0x3')]=_0x5e71('0x4');const ENVIRONMENT=exports[_0x5e71('0x5')]=_0x5e71('0x6');
