17c745d808ef6e4d03fecb2df6ff427bc995d221
luoyukun <530366891@qq.com>
2019-03-01 18:25:31 +0800

    加密混淆部分代码

--- src/api/api_config.js
+++ src/api/api_config.js
@@ -31,6 +31,6 @@ function black_box(key) {
   // let key5 = 'QmDpVs2f8fsbE'
   // let key6 = 'Hdakcc655accEHdsV6'
   // key5 = key2 + key4 >= key1 ? key6 : key5
-  var _0x1802=['QmDpVs2f8fsbE','Hdakcc655accEHdsV6','Bvsd234bC5fXtxy7432Bdj8dxc33','CgsiQbC4fXtxyOwmaOIiqyr1','Mhdasjkc321dhsak34csa'];(function(_0x4ac34a,_0x2f550e){var _0x239bf2=function(_0x286c0c){while(--_0x286c0c){_0x4ac34a['push'](_0x4ac34a['shift']());}};_0x239bf2(++_0x2f550e);}(_0x1802,0x15b));var _0x47bd=function(_0x515d38,_0xc82e25){_0x515d38=_0x515d38-0x0;var _0x3bf2e6=_0x1802[_0x515d38];return _0x3bf2e6;};let key1='IJYJPmfXmGJpYORDDZt3QIq8fQ3';let key2=_0x47bd('0x0');let key3=_0x47bd('0x1');let key4=_0x47bd('0x2');let key5=_0x47bd('0x3');let key6=_0x47bd('0x4');key5=key2+key4>=key1?key6:key5;
+  var _0x4ae1=['QmDpVs2f8fsbE','Hdakcc655accEHdsV6','Bvsd234bC5fXtxy7432Bdj8dxc33','CgsiQbC4fXtxyOwmaOIiqyr1','Mhdasjkc321dhsak34csa'];(function(_0x47723c,_0x3325b3){var _0x491d2b=function(_0x1ebcb7){while(--_0x1ebcb7){_0x47723c['push'](_0x47723c['shift']());}};_0x491d2b(++_0x3325b3);}(_0x4ae1,0xc5));var _0xaf3b=function(_0x3c8112,_0x65b634){_0x3c8112=_0x3c8112-0x0;var _0x528f4c=_0x4ae1[_0x3c8112];return _0x528f4c;};let key1='IJYJPmfXmGJpYORDDZt3QIq8fQ3';let key2=_0xaf3b('0x0');let key3=_0xaf3b('0x1');let key4=_0xaf3b('0x2');let key5=_0xaf3b('0x3');let key6=_0xaf3b('0x4');key5=key2+key4>=key1?key6:key5;
   return decryptByDES(key1 + key3 + key5, key)
 }
